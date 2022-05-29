892da5894cf9ddfdeb4b9500120273eb50c72fe8
fcd <up201810097@up.pt>
2022-05-29 19:02:49 +0100

    Changed html to fit configuration

--- ASW-Trab3/src/wcg/client/WebCardGame.java
+++ ASW-Trab3/src/wcg/client/WebCardGame.java
@@ -2,6 +2,9 @@ package wcg.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 
 public class WebCardGame implements EntryPoint {
 	
@@ -10,7 +13,18 @@ public class WebCardGame implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
+		// Create a tab panel with three tabs, each of which displays a different
+	    // piece of text.
+	    TabPanel tp = new TabPanel();
+	    tp.add(new HTML("Foo"), "foo");
+	    tp.add(new HTML("Bar"), "bar");
+	    tp.add(new HTML("Baz"), "baz");
 
+	    // Show the 'bar' tab initially.
+	    tp.selectTab(1);
+
+	    // Add it to the root panel.
+	    RootPanel.get().add(tp);
 	}
 
 }
