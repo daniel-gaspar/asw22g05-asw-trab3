165480ae9159410b0399da1cfe4d7a60f747ef44
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-06-02 13:53:06 +0100

    changed occurrences of tp to TabPanel. Changed call of UserRegistry constructor

--- ASW-Trab3/src/wcg/client/WebCardGame.java
+++ ASW-Trab3/src/wcg/client/WebCardGame.java
@@ -3,29 +3,26 @@ package wcg.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
 
 public class WebCardGame implements EntryPoint {
-	
+
 	private final CardGameServiceAsync cardGameService = GWT.create(CardGameService.class);
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
 		// Create a tab panel with three tabs, each of which displays a different
-	    // piece of text.
-	    TabPanel tp = new TabPanel();
-	    tp.add(new UserRegistry().getUserRegistry(), "Login/Registration");
-	    tp.add(new HTML(""), "Select game");
-	    tp.add(new HTML(""), "Play");
+		// piece of text.
+		TabPanel tabPanel = new TabPanel();
+		tabPanel.add(new UserRegistry(tabPanel, cardGameService).getUserRegistry(), "Login/Registration");
+		tabPanel.add(new HTML(""), "Select Game");
+		tabPanel.add(new HTML(""), "Play");
 
-	    // Show the 'bar' tab initially.
-	    tp.selectTab(0);
+		// Show the 'bar' tab initially.
+		tabPanel.selectTab(0);
 
-	    // Add it to the root panel.
-	    RootPanel.get().add(tp);
+		// Add it to the root panel.
+		RootPanel.get().add(tabPanel);
 	}
-
 }
