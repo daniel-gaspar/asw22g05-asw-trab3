6bd5e112fb64a9d1acb8acf6657e1efd608ac578
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-06-02 13:57:33 +0100

    Now extends MainPanel. Changed names of some variables. Now replaces the Select Game tab with a proper GameCreation tab

--- ASW-Trab3/src/wcg/client/UserRegistry.java
+++ ASW-Trab3/src/wcg/client/UserRegistry.java
@@ -1,74 +1,83 @@
 package wcg.client;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class UserRegistry {
+/**
+ *
+ * Extends MainPanel adding the Widget userRegistry
+ *
+ */
+public class UserRegistry extends MainPanel {
 
 	private Widget userRegistry;
 
-	public UserRegistry() {
-		this.userRegistry = OnRegisterInitialize();
+	public UserRegistry(TabPanel tabPanel, CardGameServiceAsync cardGameService) {
+		super(tabPanel, cardGameService);
+		this.userRegistry = onRegisterInitialize();
 	}
 
 	public Widget getUserRegistry() {
 		return userRegistry;
 	}
 
-	private final CardGameServiceAsync cgsa = GWT.create(CardGameService.class);
-
-	public Widget OnRegisterInitialize() {
+	public Widget onRegisterInitialize() {
 		// Create a panel to layout the widgets
-		VerticalPanel vpanel = new VerticalPanel();
-		vpanel.setSpacing(5);
+		VerticalPanel vPanel = new VerticalPanel();
+		vPanel.setSpacing(5);
 
 		// Username
-		TextBox username = new TextBox();
-		username.ensureDebugId("regUsrTxtBox");
-		vpanel.add(new HTML("Username: "));
-		vpanel.add(username);
-		
+		TextBox usernameBox = new TextBox();
+		usernameBox.ensureDebugId("regUsrTxtBox");
+		vPanel.add(new HTML("Username: "));
+		vPanel.add(usernameBox);
+
 		// Password
-		PasswordTextBox pwd = new PasswordTextBox();
-		pwd.ensureDebugId("regPwdBox");
-		vpanel.add(new HTML("Password: "));
-		vpanel.add(pwd);
-		
+		PasswordTextBox passwordBox = new PasswordTextBox();
+		passwordBox.ensureDebugId("regPwdBox");
+		vPanel.add(new HTML("Password: "));
+		vPanel.add(passwordBox);
+
 		// Add a normal button
-	    Button normalButton = new Button(
-	        "Register", new ClickHandler() {
-	          public void onClick(ClickEvent event) {
-	        	  System.out.println("username: "+username.getText().toString()+" | pwd: "+pwd.getText().toString());
-	        	  cgsa.registerPlayer(username.getText().toString(), pwd.getText().toString(), new AsyncCallback<Void>() {
-	        		
+		Button registerButton = new Button("Register", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				String username = usernameBox.getText().toString();
+				String password = passwordBox.getText().toString();
+				System.out.println("username: " + username + " | pwd: " + password);
+				cardGameService.registerPlayer(username, password, new AsyncCallback<Void>() {
+
 					@Override
 					public void onFailure(Throwable caught) {
-						vpanel.add(new HTML("Login not sucessful: "+caught.getLocalizedMessage()));
+						vPanel.add(new HTML("Login not successful: " + caught.getLocalizedMessage()));
 					}
 
 					@Override
 					public void onSuccess(Void result) {
-						vpanel.add(new HTML("Login sucessful"));
+						vPanel.add(new HTML("Login successful"));
+
+						// Removes the tab "Select Game" and replaces it with a new one
+						tabPanel.remove(1);
+						tabPanel.insert(
+								new GameCreation(tabPanel, username, password, cardGameService).getGameCreation(),
+								"Select Game", 1);
 					}
-	        		
-	        	});
-	        	
-	          }
-	        });
-	    normalButton.ensureDebugId("cwBasicButton-normal");
-	    vpanel.add(normalButton);
-				
+
+				});
+
+			}
+		});
+		registerButton.ensureDebugId("cwBasicButton-normal");
+		vPanel.add(registerButton);
 
 		// Return the panel
-		return vpanel;
+		return vPanel;
 	}
-
 }
