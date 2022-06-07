f6fa8f5bd1f975843870fcb09ed9e5c84530d9f5
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-06-07 13:24:46 +0100

    Processes events on a fixed schedule. Added getters for all private fields

--- ASW-Trab3/src/wcg/client/GamePlay.java
+++ ASW-Trab3/src/wcg/client/GamePlay.java
@@ -3,8 +3,11 @@ package wcg.client;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
 
 import wcg.shared.cards.Card;
 import wcg.shared.events.GameEndEvent;
@@ -35,27 +38,109 @@ public abstract class GamePlay extends SubPanel {
 	//To process SendCardsEvent
 	private List<Card> cardsOnHand = new ArrayList<>();
 	
-	public GamePlay(String gameId) {
+	//To schedule the processEvents routine
+	private static final int TIMER_DELAY = 5 * 1000; // 5 seconds
+	private Timer timer = new Timer();
+	private TimerTask task = new TimerTask() {
+		public void run() {
+			processEvents();
+		}
+	};
+	
+	protected GamePlay(String gameId) {
 		super(username, password);
 		this.gameId = gameId;
-		
+		timer.scheduleAtFixedRate(task, 0, TIMER_DELAY);
 	}
 	
+	/**
+	 * @return the gameId
+	 */
+	protected String getGameId() {
+		return gameId;
+	}
+
+	/**
+	 * @return the onTable
+	 */
+	protected Map<String, List<Card>> getOnTable() {
+		return onTable;
+	}
+
+	/**
+	 * @return the roundsCompleted
+	 */
+	protected int getRoundsCompleted() {
+		return roundsCompleted;
+	}
+
+	/**
+	 * @return the points
+	 */
+	protected Map<String, Integer> getPoints() {
+		return points;
+	}
+
+	/**
+	 * @return the winner
+	 */
+	protected String getWinner() {
+		return winner;
+	}
+
+	/**
+	 * @return the hasTurn
+	 */
+	protected String getHasTurn() {
+		return hasTurn;
+	}
+
+	/**
+	 * @return the mode
+	 */
+	protected String getMode() {
+		return mode;
+	}
+
+	/**
+	 * @return the cardsOnHand
+	 */
+	protected List<Card> getCardsOnHand() {
+		return cardsOnHand;
+	}
+
+	/**
+	 * @return the timer
+	 */
+	protected Timer getTimer() {
+		return timer;
+	}
+
+	/**
+	 * @return the task
+	 */
+	protected TimerTask getTask() {
+		return task;
+	}
+
+	/**
+	 * Requests a list of Recent Events from the server, and processes them
+	 */
 	private void processEvents()  {
 		cardGameService.getRecentEvents(username, password, new AsyncCallback<List<GameEvent>>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				
+				messages.setHTML(caught.getMessage());	
 			}
 
 			@Override
 			public void onSuccess(List<GameEvent> recentEvents) {
-				// TODO Auto-generated method stub
 				for(GameEvent event : recentEvents) {
 					if(event instanceof SendCardsEvent) {
 						cardsOnHand.addAll(((SendCardsEvent) event).getCards());
+						
+						messages.setHTML("Cards have been given.");
 						
 						drawCardsOnHand();
 					}
@@ -65,12 +150,16 @@ public abstract class GamePlay extends SubPanel {
 						roundsCompleted = ((RoundUpdateEvent) event).getRoundsCompleted();
 						mode = ((RoundUpdateEvent) event).getMode();
 						
+						messages.setHTML("It is now " + hasTurn + "'s turn.");
+						
 						drawCardsOnTable();
 					}
 					if(event instanceof RoundConclusionEvent) {
 						onTable = ((RoundConclusionEvent) event).getCardsOnTable();
 						roundsCompleted = ((RoundConclusionEvent) event).getRoundsCompleted();
 						points = ((RoundConclusionEvent) event).getPoints();
+						
+						messages.setHTML("Round #" + roundsCompleted + " is complete. It is now " + hasTurn + "'s turn.");
 						
 						drawCardsOnTable();
 					}
@@ -80,6 +169,8 @@ public abstract class GamePlay extends SubPanel {
 						winner = ((GameEndEvent) event).getWinner();
 						points = ((GameEndEvent) event).getPoints();
 						
+						messages.setHTML(winner + " has won the game with " + points.get(winner)  + " points.");
+						
 						drawCardsOnTable();
 					}
 				}
@@ -87,7 +178,13 @@ public abstract class GamePlay extends SubPanel {
 		});
 	}
 	
-	protected abstract void drawCardsOnHand();
+	/**
+	 * Draw the cards currently on Player's hand
+	 */
+	protected abstract Widget drawCardsOnHand();
 	
-	protected abstract void drawCardsOnTable();
+	/**
+	 * Draw the cards currently on the Table
+	 */
+	protected abstract Widget drawCardsOnTable();
 }
