fbe8a6ae8ef45068a8ba32997a119a260a7479ee
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-06-07 12:46:48 +0100

    Turned createCard into static

--- ASW-Trab3/src/wcg/client/Cards.java
+++ ASW-Trab3/src/wcg/client/Cards.java
@@ -1,7 +1,5 @@
 package wcg.client;
 
-import java.util.List;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Image;
 
@@ -11,18 +9,15 @@ import wcg.shared.cards.CardValue;
 
 public class Cards {
 	
-	protected List<Card> cards;
-	
 	private static final String FILE_EXTENSION = ".png";
 	private static final String SEPARATOR = "_of_";
 	private static final String SEPARATOR_JOKER = "_joker";
 	private static final String IMAGE_LOCATION = GWT.getModuleBaseURL() + "imgs/";
 	
-	Cards(List<Card> cards) {
-		this.cards = cards;
+	Cards() {
 	}
 
-	protected Image createCard(Card card) {
+	protected static Image createCard(Card card) {
 		String cardFile;
 		
 		if(card.getValue() == null) {
@@ -35,13 +30,13 @@ public class Cards {
 		return new Image(IMAGE_LOCATION + cardFile);
 	}
 	
-	private String getJokerColorText(CardSuit suit) {
+	private static String getJokerColorText(CardSuit suit) {
 		if(suit.equals(CardSuit.CLUBS) || suit.equals(CardSuit.SPADES))
 			return "black";
 		return "red";
 	}
 	
-	private String getValueText(CardValue value) {
+	private static String getValueText(CardValue value) {
 		if(value.equals(CardValue.ACE) || value.equals(CardValue.JACK) || value.equals(CardValue.KING) || value.equals(CardValue.QUEEN)) {
 			if(value.equals(CardValue.ACE))
 				return "ace";
@@ -55,7 +50,7 @@ public class Cards {
 		return value.toString();
 	}
 	
-	private String getSuitText(CardSuit suit) {
+	private static String getSuitText(CardSuit suit) {
 		if(suit.equals(CardSuit.CLUBS))
 			return "clubs";
 		if(suit.equals(CardSuit.DIAMONDS))
