797782cd1b18b16c3816ed3731605f55d9f598ab
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-06-09 19:24:15 +0100

    Added functionality for auto-adding bots after timeout

--- ASW-Trab3/src/wcg/client/WaitingTab.java
+++ ASW-Trab3/src/wcg/client/WaitingTab.java
@@ -3,8 +3,7 @@
  */
 package wcg.client;
 
-
-
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,14 +23,17 @@ public class WaitingTab extends SubPanel {
 	private Widget waitingTab = new HTML("Waiting for Game to Start");
 	
 	private String gameID;
+	private boolean owner_flag;
 	
 	private boolean repeat = true;
 	
 	private static final int TIMER_DELAY = 10 * 1000; //10 seconds
+	private static final int GAME_START_TIMEOUT = 60 * 1000; //1 minute
 
-	public WaitingTab(String gameID) {
+	public WaitingTab(String gameID, boolean owner) {
 		super(username, password);
 		this.gameID = gameID;
+		this.owner_flag = owner;
 		
 		Scheduler.get().scheduleFixedDelay(new Scheduler.RepeatingCommand() {
 
@@ -40,6 +42,7 @@ public class WaitingTab extends SubPanel {
 				Logger logger = Logger.getLogger("nameOfLogger");
 				logger.log(Level.SEVERE, "executing waitingTabScheduler");
 				if(repeat) verifyStartGame();
+				if(FLAG_AUTOBOTS_ROLLOUT && owner_flag && repeat) verifyAddBots();
 				return repeat;
 			}
 			
@@ -66,7 +69,7 @@ public class WaitingTab extends SubPanel {
 					String currentGameID = gameInfo.getGameId();
 					
 					if(currentGameID.equals(gameID)) {
-						if(currentPlayersCount == ("WAR".equals(currentGameName) ? 2 : 4)) {
+						if(currentPlayersCount == AuxMethods.numberOfPlayers(currentGameName)) {
 							repeat = false;
 							tabPanel.remove(2);
 							if("WAR".equals(currentGameName)) {
@@ -80,5 +83,54 @@ public class WaitingTab extends SubPanel {
 					}
 				}
 			}});
+	}
+	
+	private void verifyAddBots() {
+		cardGameService.getAvailableGameInfos(new AsyncCallback<List<GameInfo>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				messages.setHTML(caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(List<GameInfo> availableGameInfos) {
+				for(GameInfo gameInfo : availableGameInfos) {
+					String currentGameID = gameInfo.getGameId();
+					
+					if(currentGameID.equals(gameID)) {
+						Date currentLastAccessDate = gameInfo.getLastAccessDate();
+						Date nowDate = new Date();
+						
+						if((nowDate.getTime() - currentLastAccessDate.getTime()) > GAME_START_TIMEOUT) {
+							
+							String currentGameName = gameInfo.getGameName();
+							int currentPlayersCount = gameInfo.getPlayersCount();
+							for(int i = currentPlayersCount ; i < AuxMethods.numberOfPlayers(currentGameName) ; i++)
+								addBot(gameID);
+		
+						}
+					}
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Prompts the server to add a bot to a Game with gameID
+	 * 
+	 * @param gameID - of game
+	 */
+	private void addBot(String gameID) {
+		cardGameService.addBotPlayer(gameID, new AsyncCallback<Void>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				messages.setHTML(caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(Void result) {
+				messages.setHTML("Bot added");
+			}
+		});
 	}
 }
