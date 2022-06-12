3917250c940564b517b72ecf4c8659a061f0db0e
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-06-12 17:58:38 +0100

    made changes to be able to set CSS properties for text and tabPanel

--- ASW-Trab3/src/wcg/client/WebCardGame.java
+++ ASW-Trab3/src/wcg/client/WebCardGame.java
@@ -2,9 +2,11 @@ package wcg.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.DeckPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabBar;
 
 public class WebCardGame implements EntryPoint {
 
@@ -27,18 +29,31 @@ public class WebCardGame implements EntryPoint {
 		// piece of text.
 		tabPanel.add(new UserRegistry(tabPanel, cardGameService, systemMessages).getUserRegistry(),
 				"Login/Registration");
-		tabPanel.add(new HTML("Not logged in yet"), "Select Game");
+
+		HorizontalPanel selectGameNotLoggedIn = new HorizontalPanel();
+		selectGameNotLoggedIn.setStyleName("wcg-Panel");
+		HTML notLoggedIn = new HTML("Not logged in yet");
+		notLoggedIn.setStyleName("wcg-Text");
+		selectGameNotLoggedIn.add(notLoggedIn);
+		tabPanel.add(selectGameNotLoggedIn, "Select Game");
 
 		// Show the 'Login/Registry' tab initially.
 		tabPanel.selectTab(0);
 
 		// Create a panel for System Messages (caught errors, and others)
 		systemMessagesPanel.setSpacing(5);
+		systemMessagesStatic.setStyleName("wcg-Text");
+		systemMessages.setStyleName("wcg-Text");
 		systemMessagesPanel.add(systemMessagesStatic);
 		systemMessagesPanel.add(systemMessages);
 
 		// Add the elements to the root panel
 		rootPanel.add(systemMessagesPanel);
+
+		TabBar tabBar = tabPanel.getTabBar();
+		DeckPanel deckPanel = tabPanel.getDeckPanel();
+		tabBar.addStyleName("wcg-TabPanelBar");
+		deckPanel.addStyleName("wcg-TabPanelDeck");
 		rootPanel.add(tabPanel);
 	}
 }
